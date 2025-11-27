907805dba430c2d3784aa35b0a4cbf01d5f9bfde
Iván Vázquez Martín <ivanvmartin@gmail.com>
2025-11-27 09:50:32 +0100

    Estructura de proyecto realizada

--- src/main/java/pokemon/Main.java
+++ src/main/java/pokemon/Main.java
@@ -3,7 +3,36 @@ package pokemon;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* --> https://pokeapi.co/api/v2/pokedex/?/ -- ? = numero pokedex
+		 * POKEDEX:
+		 * name string
+		 * pokemon_entries list PokemonAuxiliar
+		 * 
+		 * PARA LA LISTA:
+		 * PokemonAuxiliar.entry_number --> numero pokedex del pokemon
+		 * PokemonAuxiliar.pokemon_species.name --> nombre de pokemon
+		 * 
+		 * POKEMONAUXILIAR:
+		 * entry_number int
+		 * pokemon_species PokemonSpecies
+		 * 
+		 * POKEMONSPECIES
+		 * name string
+		 * 
+		 * ---> END POINT: https://pokeapi.co/api/v2/pokemon/[name] --> datos pokemon
+		 * 
+		 * POKEMON
+		 * name string
+		 * height int
+		 * weight int
+		 * types list NumType
+		 * 
+		 * NUMTYPE:
+		 * type Type
+		 * 
+		 * TYPE:
+		 * name string
+		 */
 
 	}
 
