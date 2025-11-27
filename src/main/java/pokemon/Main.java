23502f23e724cf63f3f1526b4fd2cbbe43e7f80f
Iván Vázquez Martín <ivan.vazquez@live.u-tad.com>
2025-11-27 17:42:45 +0100

    Rest api (hasta pokemon) terminado. Falta tipo.

--- src/main/java/pokemon/Main.java
+++ src/main/java/pokemon/Main.java
@@ -1,6 +1,18 @@
 package pokemon;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /* 
  * --> https://pokeapi.co/api/v2 --> API Pokemon
@@ -10,12 +22,12 @@ import java.util.HashMap;
  * pokemon string --> url Para pokemon: https://pokeapi.co/api/v2/pokemon/
  * 
  * APIPOKEDEX - https://pokeapi.co/api/v2/pokedex/
- * results list PokedexAuxiliar
+ * results list PokedexAuxiliar --> get(0) --> Pokedex nacional con TODOS LOS POKEMON
  * 
  * POKEDEXAUXILIAR
- * url string --> https://pokeapi.co/api/v2/pokedex/?/ -- ? = numero pokedex
+ * url string --> https://pokeapi.co/api/v2/pokedex/1 --> numero pokedex
  * 
- * POKEDEX: https://pokeapi.co/api/v2/pokedex/?/
+ * POKEDEX: https://pokeapi.co/api/v2/pokedex/1
  * name string
  * pokemon_entries list PokemonAuxiliar
  * 
@@ -46,11 +58,88 @@ import java.util.HashMap;
  */
 
 public class Main {
+	
+	static HashMap<String, Object> cache = new HashMap<>(); // caché de los end points
+	static String api = "https://pokeapi.co/api/v2";
+	static ObjectMapper mapper = new ObjectMapper();
+	static HttpClient cliente = HttpClient.newHttpClient();
+	static HttpRequest peticion;
+	static HttpResponse<String> respuesta;
+	static Scanner sc = new Scanner(System.in);
+	static String json;
+	static String url;
 
 	public static void main(String[] args) {
 
-		HashMap<String, Object> cache; // caché de los end points
+		try {
+			apiRest();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
 		
+	}
+
+	private static void apiRest()
+			throws IOException, InterruptedException, JsonProcessingException, JsonMappingException {
+		// ----------- APIPOKEMON -----------
+		ApiPokemon miApi = new ApiPokemon();
+		url = api;
+		if (!cache.containsKey(url)) {
+			peticion = HttpRequest.newBuilder().uri(URI.create(url)).build();
+			respuesta = cliente.send(peticion, BodyHandlers.ofString());
+			json = respuesta.body();
+			miApi = mapper.readValue(json, ApiPokemon.class);
+			cache.put(api, miApi);
+		} else
+			miApi = (ApiPokemon) cache.get(url);
+		// ----------- APIPOKEDEX -----------
+		ApiPokedex miApiPokedex = new ApiPokedex();
+		url = miApi.getPokedex();
+		if (!cache.containsKey(url)) {
+			peticion = HttpRequest.newBuilder().uri(URI.create(url)).build();
+			respuesta = cliente.send(peticion, BodyHandlers.ofString());
+			json = respuesta.body();
+			miApiPokedex = mapper.readValue(json, ApiPokedex.class);
+			cache.put(url, miApiPokedex);
+		} else
+			miApiPokedex = (ApiPokedex) cache.get(url);
+		// AGREGAMOS POKEDEX NACIONAL
+		// ----------- POKEDEX -----------
+		Pokedex miPokedex = new Pokedex();
+		url = miApiPokedex.getResults().get(0).getUrl(); // URL POKEDEX NACIONAL
+		if (!cache.containsKey(url)) {
+			peticion = HttpRequest.newBuilder().uri(URI.create(url)).build();
+			respuesta = cliente.send(peticion, BodyHandlers.ofString());
+			json = respuesta.body();
+			miPokedex = mapper.readValue(json, Pokedex.class);
+			cache.put(url, miPokedex);
+		} else
+			miPokedex = (Pokedex) cache.get(url);
+
+		// POKEMON ALEATORIO AL AZAR
+		Random idRandom = new Random();
+		int id = idRandom.nextInt(miPokedex.getPokemon_entries().size()); // Cogemos un pokemon al azar
+		String nombrePokemon = miPokedex.getPokemon_entries().get(id).getPokemon_species().getName(); // Cogemos su
+																										// nombre
+		// ----------- POKEMON -----------
+		Pokemon miPokemon = new Pokemon();
+		url = miApi.getPokemon() + nombrePokemon; // ENDPOINT PARA DATOS DEL POKEMON -->
+													// https://pokeapi.co/api/v2/pokemon/[nombrePokemon]
+		if (!cache.containsKey(url)) {
+			peticion = HttpRequest.newBuilder().uri(URI.create(url)).build();
+			respuesta = cliente.send(peticion, BodyHandlers.ofString());
+			json = respuesta.body();
+			miPokemon = mapper.readValue(json, Pokemon.class);
+			cache.put(url, miPokedex);
+		} else
+			miPokemon = (Pokemon) cache.get(url);
+
+		// PRUEBA
+		System.out.println(miPokemon.toString());
 	}
 
 }
